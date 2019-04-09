26e3c871c42093a64c83033fd2c7c4609092c46a
René Monroy <rene.dev.js@gmail.com>
2019-04-09 17:04:17 -0500

    Change hyper font size

--- hyper/.hyper.js
+++ hyper/.hyper.js
@@ -1,7 +1,7 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 13,
+    fontSize: 12,
 
     // font family with optional fallbacks
     fontFamily:
@@ -30,7 +30,7 @@ module.exports = {
     termCSS: "",
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: "12px 14px",
+    padding: "14px",
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
