0496021e1717fe6f8e66f634223d65059adbc356
René Monroy <rene.dev.js@gmail.com>
2019-04-09 19:36:41 -0500

    Install slack

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo '[1/10] Installing Homebrew...'
+echo '[1/11] Installing Homebrew...'
 echo 'This will install Command Line Tools if needed (it may take a while)'
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-echo '[2/10] Installing git...'
+echo '[2/11] Installing git...'
 brew update
 brew upgrade
 brew install git
@@ -15,27 +15,27 @@ DOTFILES_PATH=$(pwd)
 ln -sf $DOTFILES_PATH/git/.gitconfig ~/.gitconfig
 ln -sf $DOTFILES_PATH/git/.gitignore_global ~/.gitignore_global
 
-echo '[3/10] Installing zsh...'
+echo '[3/11] Installing zsh...'
 brew install zsh
 curl -sL git.io/antibody | sh -s
 ln -sf $DOTFILES_PATH/zsh/.zshrc ~/.zshrc
 grep -q -f $(which zsh) /etc/shells || echo $(which zsh) | sudo tee -a /etc/shells
 chsh -s $(which zsh)
 
-echo '[4/10] Installing node with nvm...'
+echo '[4/11] Installing node with nvm...'
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install node
 
-echo '[5/10] Installing silver searcher (ag)...'
+echo '[5/11] Installing silver searcher (ag)...'
 brew install the_silver_searcher
 
-echo '[6/10] Installing fzy...'
+echo '[6/11] Installing fzy...'
 brew install fzy
 
-echo '[7/10] Installing hyper...'
+echo '[7/11] Installing hyper...'
 brew cask install hyper
 rm -rf ~/.hyper.js
 ln -sf $DOTFILES_PATH/hyper/.hyper.js ~/.hyper.js
@@ -45,7 +45,7 @@ cd ~/Development/fonts
 cd ..
 rm -rf ./fonts
 
-echo '[8/10] Installing vscode and some extensions...'
+echo '[8/11] Installing vscode and some extensions...'
 brew cask install visual-studio-code
 VSCODE_EXTENSIONS=(
   dbaeumer.vscode-eslint
@@ -62,12 +62,13 @@ for VSCODE_EXT in "${VSCODE_EXTENSIONS[@]}"
   do code --install-extension $VSCODE_EXT
 done
 
-echo '[9/10] Installing chrome...'
+echo '[9/11] Installing chrome...'
 brew cask install google-chrome
 
-echo '[10/10] Setting up system configurations...'
+echo '[10/11] Installing slack...'
+
+echo '[11/11] Setting up system configurations...'
 brew cleanup
-defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.dock autohide -bool true
 defaults write NSGlobalDomain AppleInterfaceStyle Dark
